8f42fc234e59a6738b121c6ba6639df2c5b2fd09
vaidika03 <69469578+vaidika03@users.noreply.github.com>
2024-11-25 23:20:42 +0530

    Create Bubble_sort.py

---
+++ Bubble_sort.py
@@ -0,0 +1,17 @@
+#BUBBLE SORT : placing the first largest element at its correct position. It repeatedly swaps adjacent elements to sort a list..
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(n-i-1):
+            if(arr[j]>arr[j+1]):
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+                swapped = True
+        if swapped == False:
+            break
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    bubble_sort(arr)
+    print(arr)               
