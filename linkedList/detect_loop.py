54a76e133e20aaa86ffde3600e5ee4d4cb86f946
vaidika03 <69469578+vaidika03@users.noreply.github.com>
2025-02-17 21:29:53 +0530

    8 Detect a cycle in Linked List

---
+++ linkedList/detect_loop.py
@@ -0,0 +1,20 @@
+#8 Detect a cycle in Linked List
+def detect_loop(head):
+    slow = head
+    fast = head
+    while slow and fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+if __name__ =="__main__":
+    head = Node(10)
+    head.next = Node(20)
+    head.next.next = Node(30)
+    head.next.next.next = Node(40)
+    head.next.next.next.next = Node(50)
+    head.next.next.next.next.next = Node(60)
+    head.next.next.next.next = head
+    print("8. detect_loop:",detect_loop(head))
